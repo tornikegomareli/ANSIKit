611fc1e7c22f79884f1e33f66650010e30631da6
tornikegomareli <gomarelidevelopment@gmail.com>
2023-09-30 23:50:52 +0400

    DoCc comments

--- Sources/Core/ANSIAttr/ANSIAttributeState.swift
+++ Sources/Core/ANSIAttr/ANSIAttributeState.swift
@@ -1,11 +1,14 @@
-//
 //  ANSIAttributeState.swift
-//
 //
 //  Created by Tornike on 30/09/2023.
 //
 
 import Foundation
 
+/// Indicates whether any ANSI foreground or background color attributes are currently open.
+/// - Note: An "open" state means that a color attribute has been set but not yet reset to default.
 internal var isOpenedColor = false
+
+/// Indicates whether any ANSI text style attributes are currently open.
+/// - Note: An "open" state means that a style attribute has been set but not yet reset to default.
 internal var isOpenedStyle = false
