e83a7b0cc5c4ca3375e94e1dd22be51d89ff4406
tornikegomareli <gomarelidevelopment@gmail.com>
2023-09-30 23:57:25 +0400

    Formatting

--- Sources/Extensions/String+ANSII+Colors.swift
+++ Sources/Extensions/String+ANSII+Colors.swift
@@ -1,313 +1,365 @@
 //
 //  String+ANSII+Colors.swift
-//  
+//
 //
 //  Created by Tornike on 30/09/2023.
 //
 
 import Foundation
 
+/// An extension for applying ANSI colors to strings.
 public extension String {
+  /// Returns the string with black ANSI color.
   var black: String {
     return color(
       .black
     )
   }
 
+  /// Returns the string with red ANSI color.
   var red: String {
     return color(
       .red
     )
   }
 
+  /// Returns the string with green ANSI color.
   var green: String {
     return color(
       .green
     )
   }
 
+  /// Returns the string with brown ANSI color.
   var brown: String {
     return color(
       .brown
     )
   }
 
+  /// Returns the string with blue ANSI color.
   var blue: String {
     return color(
       .blue
     )
   }
-  
+
+  /// Returns the string with magenta ANSI color.
   var magenta: String {
     return color(
       .magenta
     )
   }
 
+  /// Returns the string with cyan ANSI color.
   var cyan: String {
     return color(
       .cyan
     )
   }
 
+  /// Returns the string with gray ANSI color.
   var gray: String {
     return color(
       .gray
     )
   }
 
+  /// Returns the string with default ANSI color.
   var `default`: String {
     return color(
       .`default`
     )
   }
 
+  /// Returns the string with dark gray ANSI color.
   var darkGray: String {
     return color(
       .darkGray
     )
   }
 
+  /// Returns the string with light red ANSI color.
   var lightRed: String {
     return color(
       .lightRed
     )
   }
 
+  /// Returns the string with light green ANSI color.
   var lightGreen: String {
     return color(
       .lightGreen
     )
   }
 
+  /// Returns the string with yellow ANSI color.
   var yellow: String {
     return color(
       .yellow
     )
   }
 
+  /// Returns the string with light blue ANSI color.
   var lightBlue: String {
     return color(
       .lightBlue
     )
   }
 
+  /// Returns the string with light magenta ANSI color.
   var lightMagenta: String {
     return color(
       .lightMagenta
     )
   }
 
+  /// Returns the string with light cyan ANSI color.
   var lightCyan: String {
     return color(
       .lightCyan
     )
   }
 
+  /// Returns the string with white ANSI color.
   var white: String {
     return color(
       .white
     )
   }
 
+  /// Returns the string with onBlack ANSI background color.
   var onBlack: String {
     return color(
       .onBlack
     )
   }
 
+  /// Returns the string with onRed ANSI background color.
   var onRed: String {
     return color(
       .onRed
     )
   }
 
+  /// Returns the string with onGreen ANSI background color.
   var onGreen: String {
     return color(
       .onGreen
     )
   }
 
+  /// Returns the string with onBrown ANSI background color.
   var onBrown: String {
     return color(
       .onBrown
     )
   }
 
+  /// Returns the string with onBlue ANSI background color.
   var onBlue: String {
     return color(
       .onBlue
     )
   }
 
+  /// Returns the string with onMagenta ANSI background color.
   var onMagenta: String {
     return color(
       .onMagenta
     )
   }
 
+  /// Returns the string with onCyan ANSI background color.
   var onCyan: String {
     return color(
       .onCyan
     )
   }
 
+  /// Returns the string with onGray ANSI background color.
   var onGray: String {
     return color(
       .onGray
     )
   }
 
+  /// Returns the string with onDefault ANSI background color.
   var onDefault: String {
     return color(
       .onDefault
     )
   }
 
+  /// Returns the string with onDarkGray ANSI background color.
   var onDarkGray: String {
     return color(
       .onDarkGray
     )
   }
 
+  /// Returns the string with onLightRed ANSI background color.
   var onLightRed: String {
     return color(
       .onLightRed
     )
   }
 
+  /// Returns the string with onLightGreen ANSI background color.
   var onLightGreen: String {
     return color(
       .onLightGreen
     )
   }
 
+  /// Returns the string with onYellow ANSI background color.
   var onYellow: String {
     return color(
       .onYellow
     )
   }
 
+  /// Returns the string with onLightBlue ANSI background color.
   var onLightBlue: String {
     return color(
       .onLightBlue
     )
   }
 
+  /// Returns the string with onLightMagenta ANSI background color.
   var onLightMagenta: String {
     return color(
       .onLightMagenta
     )
   }
 
+  /// Returns the string with onLightCyan ANSI background color.
   var onLightCyan: String {
     return color(
       .onLightCyan
     )
   }
 
+  /// Returns the string with onWhite ANSI background color.
   var onWhite: String {
     return color(
       .onWhite
     )
   }
 
+  /// Returns the string with black ANSI color.
   var asBlack: String {
     return color(
       .black
     )
   }
 
+  /// Returns the string with red ANSI color.
   var asRed: String {
     return color(
       .red
     )
   }
 
+  /// Returns the string with green ANSI color.
   var asGreen: String {
     return color(
       .green
     )
   }
 
+  /// Returns the string with brown ANSI color.
   var asBrown: String {
     return color(
       .brown
     )
   }
 
+  /// Returns the string with blue ANSI color.
   var asBlue: String {
     return color(
       .blue
     )
   }
 
+  /// Returns the string with magenta ANSI color.
   var asMagenta: String {
     return color(
       .magenta
     )
   }
 
+  /// Returns the string with cyan ANSI color.
   var asCyan: String {
     return color(
       .cyan
     )
   }
 
+  /// Returns the string with gray ANSI color.
   var asGray: String {
     return color(
       .gray
     )
   }
 
+  /// Returns the string with default ANSI color.
   var asDefault: String {
     return color(
       .`default`
     )
   }
 
+  /// Returns the string with dark gray ANSI color.
   var asDarkGray: String {
     return color(
       .darkGray
     )
   }
 
+  /// Returns the string with light red ANSI color.
   var asLightRed: String {
     return color(
       .lightRed
     )
   }
 
+  /// Returns the string with light green ANSI color.
   var asLightGreen: String {
     return color(
       .lightGreen
     )
   }
 
+  /// Returns the string with yellow ANSI color.
   var asYellow: String {
     return color(
       .yellow
     )
   }
 
+  /// Returns the string with light blue ANSI color.
   var asLightBlue: String {
     return color(
       .lightBlue
     )
   }
 
+  /// Returns the string with light magenta ANSI color.
   var asLightMagenta: String {
     return color(
       .lightMagenta
     )
   }
 
+  /// Returns the string with light cyan ANSI color.
   var asLightCyan: String {
     return color(
       .lightCyan
     )
   }
 
+  /// Returns the string with white ANSI color.
   var asWhite: String {
     return color(
       .white
